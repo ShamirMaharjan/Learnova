39d6e46c0e111d29dfd11930903773b6dc910a9d
Shamir Maharjan <shamirmaharjan07@gmail.com>
2025-06-06 18:05:39 +0545

    fix deployment issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import CTA from '@/components/CTA'
 import { getAllCompanions, getRecentSessions } from "@/lib/actions/companion.actions";
 import { getSubjectColor } from "@/lib/utils";
 import CompanionCard from '@/components/CompanionCard';
+export const dynamic = 'force-dynamic'
+
 const Page = async () => {
   const companions = await getAllCompanions({ limit: 3 });
   const recentSessionsCompanions = await getRecentSessions(10);
